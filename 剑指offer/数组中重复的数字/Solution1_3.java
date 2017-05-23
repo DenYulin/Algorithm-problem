46cc76bf050aec5f40368b18bed2ef9bc716f28c
dylin <test@qq.com>
2017-05-23 21:56:54 +0800

    数组中重复的数字
    提交了  题目一：找不数组中重复的数字   的解决方案和替代实现

---
+++ 剑指offer/数组中重复的数字/Solution1_3.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean duplicate(int numbers[],int length,int [] duplication) {
+        if (numbers == null || length <= 0){
+            return false;
+        }
+        for (int i = 0; i < length; i++){
+            int m = numbers[i];
+            while (numbers[i] != i){
+                if (numbers[i] == numbers[numbers[i]]){
+                    duplication[0] = numbers[i];
+                    return true;
+                } else {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[temp];
+                    numbers[temp] = temp;
+                }
+            }
+        }
+        return false;
+    }
+}
